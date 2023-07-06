9b02aeb7124c9c45f42d522747ad1cca870ab0b4
jaynel29 <jaynellucanas@gmail.com>
2023-07-06 23:19:57 +0800

    Finished

--- src/pages/Messages/MessageComponents/ConversationContainer.tsx
+++ src/pages/Messages/MessageComponents/ConversationContainer.tsx
@@ -88,7 +88,7 @@ const ConversationContainer = () => {
     }
 
     return (
-        <div className={`${focused ? 'absolute' : 'fixed '} sm:sticky top-0 bottom-0 dark:bg-Dark100 bg-slate-100 w-full overflow-hidden flex flex-col lg:border-l dark:border-Dark300`}>
+        <div className={`${focused ? 'absolute' : 'fixed '} sm:sticky top-0 bottom-0 dark:bg-Dark100 bg-slate-100 w-full overflow-y-auto flex flex-col lg:border-l dark:border-Dark300`}>
             {conversationInfo && (
                 <div className='flex flex-col flex-grow'>
                     <Header>
