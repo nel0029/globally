b17a59367f06de324c22441326b7e3cdb640c453
jaynel29 <jaynellucanas@gmail.com>
2023-07-07 10:34:04 +0800

    Finished

--- src/pages/Messages/MessageComponents/ConversationContainer.tsx
+++ src/pages/Messages/MessageComponents/ConversationContainer.tsx
@@ -130,7 +130,7 @@ const ConversationContainer = () => {
 
 
                     </div>
-                    <div className="sticky bottom-0 w-full flex flex-row px-2 flex-shrink overflow-x-hidden">
+                    <div className="sticky bottom-0 dark:bg-Dark100 bg-slate-100 w-full flex flex-row px-2 flex-shrink overflow-x-hidden">
                         <div className="flex-grow">
                             <input
                                 placeholder="Aa"
