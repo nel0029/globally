7234df94a7143892edf8eabc1b7fc393189580b1
jaynel29 <jaynellucanas@gmail.com>
2023-07-07 11:05:58 +0800

    Finished

--- src/pages/Home/PostComponents/PollOptionsCard.tsx
+++ src/pages/Home/PostComponents/PollOptionsCard.tsx
@@ -58,7 +58,7 @@ const PollOptionsCard: React.FC<PollOptionListProps> = ({ options, postID, hasCh
                         className='peer sr-only'
                     />
                     <div className={`${option._id === optionChoosedID ? 'text-secondary1' : ''} z-10 flex items-center justify-center`}>
-                        <IonIcon name={`${option._id === optionChoosedID ? checkmarkCircle : checkmarkCircleOutline}`} />
+                        <IonIcon icon={`${option._id === optionChoosedID ? checkmarkCircle : checkmarkCircleOutline}`} />
                     </div>
                     <div
                         onClick={(event: any) => event.stopPropagation()}
