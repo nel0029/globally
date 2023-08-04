2fc0cb240030ad8217a61ff4120e0f313c1e17f8
jaynel29 <jaynellucanas@gmail.com>
2023-08-04 22:01:45 +0800

    BgColor for post is added

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -4,4 +4,4 @@ export const developmentAddress = "http://127.0.0.1:5000";
 
 export const producionAddress = "https://globally-express.onrender.com";
 
-export const serverAddress = developmentAddress;
+export const serverAddress = producionAddress;
