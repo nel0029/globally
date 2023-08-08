38c276084422d2c5d5d89eca0490d6269ac2dff6
jaynel29 <jaynellucanas@gmail.com>
2023-08-08 09:54:02 +0800

    Message Components is been updated

--- src/pages/Messages/MessageComponents/ConversationContainer.tsx
+++ src/pages/Messages/MessageComponents/ConversationContainer.tsx
@@ -1,154 +1,168 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { useParams, useNavigate } from 'react-router'
-import { useSelector, useDispatch } from 'react-redux'
-import socket from '../../../sockets/socket'
-import Header from '../../../common/Header'
-import { IonIcon } from '@ionic/react'
-import { arrowBackOutline, paperPlane } from 'ionicons/icons'
-import MessageBubble from './MessageBubble'
-import CardAvatar from '../../Home/PostComponents/CardAvatar'
-import { getConversationInfo, getAllMessages, getUnseenMessagesCount } from '../../../redux/asynActions/messageAsyncActions'
-import { AppDispatch } from '../../../redux/store'
-import { MessageDataProps, createNewConvo, updateConvo } from '../../../redux/messageSlice'
-import { useLocation } from 'react-router'
-import { createNewMessage } from '../../../redux/messageSlice'
+/** @format */
 
+import React, { useEffect, useState, useRef } from "react";
+import { useParams, useNavigate } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import socket from "../../../sockets/socket";
+import Header from "../../../common/Header";
+import { IonIcon } from "@ionic/react";
+import { arrowBackOutline, paperPlane } from "ionicons/icons";
+import MessageBubble from "./MessageBubble";
+import CardAvatar from "../../Home/PostComponents/CardAvatar";
+import {
+  getConversationInfo,
+  getAllMessages,
+  getUnseenMessagesCount,
+} from "../../../redux/asynActions/messageAsyncActions";
+import { AppDispatch } from "../../../redux/store";
+import {
+  MessageDataProps,
+  createNewConvo,
+  updateConvo,
+} from "../../../redux/messageSlice";
+import { useLocation } from "react-router";
+import { createNewMessage } from "../../../redux/messageSlice";
 
 const ConversationContainer = () => {
-    const { conversationID } = useParams<string>()
-    const user = useSelector((state: any) => state.user.userData)
-    const dispatch = useDispatch<AppDispatch>()
-    const navigate = useNavigate();
-    const messageContainerRef = useRef<HTMLDivElement | null>(null);
-    const conversationInfo = useSelector((state: any) => state.messages.conversationInfo) || {}
-    const messages = useSelector((state: any) => state.messages.messages) || [];
-    const location = useLocation()
-    const [messageText, setMessageText] = useState("")
-    const [focused, setFocused] = useState(false)
+  const { conversationID } = useParams<string>();
+  const user = useSelector((state: any) => state.user.userData);
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const messageContainerRef = useRef<HTMLDivElement | null>(null);
+  const conversationInfo =
+    useSelector((state: any) => state.messages.conversationInfo) || {};
+  const messages = useSelector((state: any) => state.messages.messages) || [];
+  const location = useLocation();
+  const [messageText, setMessageText] = useState("");
+  const [focused, setFocused] = useState(false);
 
-    useEffect(() => {
-        const data = {
-            conversationID: conversationID,
-            userID: user.userID
-        };
-
-        dispatch(getConversationInfo(data))
-            .then(() => dispatch(getAllMessages(data)))
-    }, [conversationID]);
-
-
-    useEffect(() => {
-        scrollToBottom();
-    }, [messages]);
-
-    useEffect(() => {
-        const data = {
-            conversationID: conversationID,
-            memberID: user.userID
-        }
-
-        socket.emit('joinConversation', data);
-
-        return () => {
-            socket.emit("leaveConversation", data)
-        }
-    }, [location.pathname])
-
-    useEffect(() => {
-        const data = {
-            userID: user.userID
-        }
-        dispatch(getUnseenMessagesCount(data))
-    }, [])
-
-
-    const goBack = () => navigate('/messages');
-
-    const scrollToBottom = () => {
-        if (messageContainerRef.current) {
-            messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
-        }
+  useEffect(() => {
+    const data = {
+      conversationID: conversationID,
+      userID: user.userID,
     };
 
-    const handleSendMessage = () => {
+    dispatch(getConversationInfo(data)).then(() =>
+      dispatch(getAllMessages(data))
+    );
+  }, [conversationID]);
 
-        const data = {
-            text: messageText,
-            conversationID: conversationID,
-            senderID: user.userID
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
-        }
-
-        socket.emit("sendMessage", data)
-        scrollToBottom();
-        setMessageText('')
+  useEffect(() => {
+    const data = {
+      conversationID: conversationID,
+      memberID: user.userID,
     };
-    const handleFocus = () => {
-        setFocused(!focused)
+
+    socket.emit("joinConversation", data);
+
+    return () => {
+      socket.emit("leaveConversation", data);
+    };
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const data = {
+      userID: user.userID,
+    };
+    dispatch(getUnseenMessagesCount(data));
+  }, []);
+
+  const goBack = () => navigate("/messages");
+
+  const scrollToBottom = () => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight;
     }
+  };
 
-    return (
-        <div className={`${focused ? 'absolute' : 'fixed '} sm:sticky top-0 bottom-0 dark:bg-Dark100 bg-slate-100 w-full overflow-y-auto flex flex-col lg:border-l dark:border-Dark300`}>
-            {conversationInfo && (
-                <div className='flex flex-col flex-grow'>
-                    <Header>
-                        <div className='flex flex-row items-center '>
-                            <div className='flex-[1] flex flex-row items-center'>
-                                <div
-                                    onClick={goBack}
-                                    className="flex lg:hidden justify-center items-center p-2 rounded-full hover:bg-slate-200 dark:hover:bg-Dark300 cursor-pointer">
-                                    <IonIcon icon={arrowBackOutline} />
-                                </div>
-                                <div className='flex flex-row gap-x-2'>
-                                    <CardAvatar
-                                        width="32px"
-                                        height="32px"
-                                        avatarURL={conversationInfo.avatarURL?.url}
-                                    />
-                                    <div className='flex flex-row gap-x-1'>
-                                        <span>{conversationInfo.receiverFirstName}</span>
-                                        <span>{conversationInfo.receiverMiddleName}</span>
-                                        <span>{conversationInfo.receiverLastName}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Header>
-                    <div className="flex flex-col flex-[1] p-2 gap-y-2 overflow-y-auto" ref={messageContainerRef}>
-                        {messages && messages.length > 0 ? (
-                            messages.map((message: MessageDataProps) => (
-                                <MessageBubble
-                                    key={message._id}
-                                    senderID={message.senderID}
-                                    text={message.text}
-                                    createdAt={message.createdAt}
-                                />
-                            ))
-                        ) : (
-                            <div>No messages</div>
-                        )}
+  const handleSendMessage = () => {
+    const data = {
+      text: messageText,
+      conversationID: conversationID,
+      senderID: user.userID,
+    };
 
+    socket.emit("sendMessage", data);
+    scrollToBottom();
+    setMessageText("");
+  };
+  const handleFocus = () => {
+    setFocused(!focused);
+  };
 
-                    </div>
-                    <div className="sticky bottom-0 dark:bg-Dark100 bg-slate-100 w-full flex flex-row px-2 flex-shrink overflow-x-hidden">
-                        <div className="flex-grow">
-                            <input
-                                placeholder="Aa"
-                                className="w-full bg-transparent border dark:border-Dark300 outline-none p-2 rounded-lg"
-                                type="text"
-                                value={messageText}
-                                onChange={(event: any) => setMessageText(event.target.value)}
-                            />
-                        </div>
-                        <button className="p-2 flex flex-row items-center gap-x-2" onClick={handleSendMessage}>
-                            <IonIcon icon={paperPlane} />
-                        </button>
-                    </div>
+  return (
+    <div
+      className={` flex-grow dark:bg-Dark100 bg-slate-100 w-full overflow-y-auto flex flex-col lg:border-l dark:border-Dark300`}
+    >
+      {conversationInfo && (
+        <div className="flex flex-col flex-grow">
+          <Header>
+            <div className="flex flex-row items-center ">
+              <div className="flex-[1] flex flex-row items-center">
+                <div
+                  onClick={goBack}
+                  className="flex lg:hidden justify-center items-center p-2 rounded-full hover:bg-slate-200 dark:hover:bg-Dark300 cursor-pointer"
+                >
+                  <IonIcon icon={arrowBackOutline} />
                 </div>
+                <div className="flex flex-row gap-x-2">
+                  <CardAvatar
+                    width="32px"
+                    height="32px"
+                    avatarURL={conversationInfo.avatarURL?.url}
+                  />
+                  <div className="flex flex-row gap-x-1">
+                    <span>{conversationInfo.receiverFirstName}</span>
+                    <span>{conversationInfo.receiverMiddleName}</span>
+                    <span>{conversationInfo.receiverLastName}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Header>
+          <div
+            className="flex flex-col flex-[1] p-2 gap-y-2 overflow-y-auto"
+            ref={messageContainerRef}
+          >
+            {messages && messages.length > 0 ? (
+              messages.map((message: MessageDataProps) => (
+                <MessageBubble
+                  key={message._id}
+                  senderID={message.senderID}
+                  text={message.text}
+                  createdAt={message.createdAt}
+                />
+              ))
+            ) : (
+              <div>No messages</div>
             )}
+          </div>
+          <div className="sticky bottom-0 dark:bg-Dark100 bg-slate-100 w-full flex flex-row px-2 flex-shrink overflow-x-hidden">
+            <div className="flex-grow">
+              <input
+                placeholder="Aa"
+                className="w-full bg-transparent border dark:border-Dark300 outline-none p-2 rounded-lg"
+                type="text"
+                value={messageText}
+                onChange={(event: any) => setMessageText(event.target.value)}
+              />
+            </div>
+            <button
+              className="p-2 flex flex-row items-center gap-x-2"
+              onClick={handleSendMessage}
+            >
+              <IonIcon icon={paperPlane} />
+            </button>
+          </div>
         </div>
+      )}
+    </div>
+  );
+};
 
-    )
-}
-
-export default ConversationContainer
+export default ConversationContainer;
--- src/pages/Messages/Messages.tsx
+++ src/pages/Messages/Messages.tsx
@@ -1,136 +1,144 @@
-import React, { useEffect, useState } from 'react';
-import { Outlet, useParams, Route, Link, useLocation, Routes, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { AppDispatch } from '../../redux/store';
-import socket from '../../sockets/socket';
-import ConversationListContainer from './MessageComponents/ConversationListContainer';
-import ConversationContainer from './MessageComponents/ConversationContainer';
-import NewConversation from './MessageComponents/NewConversation';
-import MessageRequestListContainer from './MessageComponents/MessageRequestListContainer';
-import NoConvoSelected from './MessageComponents/NoConvoSelected';
-import NoMessageRequestSelected from './MessageComponents/NoMessageRequestSelected';
-import { createNewConvo, createNewMessage, updateConvo } from '../../redux/messageSlice';
+/** @format */
+
+import React, { useEffect, useState } from "react";
+import {
+  Outlet,
+  useParams,
+  Route,
+  Link,
+  useLocation,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store";
+import socket from "../../sockets/socket";
+import ConversationListContainer from "./MessageComponents/ConversationListContainer";
+import ConversationContainer from "./MessageComponents/ConversationContainer";
+import NewConversation from "./MessageComponents/NewConversation";
+import MessageRequestListContainer from "./MessageComponents/MessageRequestListContainer";
+import NoConvoSelected from "./MessageComponents/NoConvoSelected";
+import NoMessageRequestSelected from "./MessageComponents/NoMessageRequestSelected";
+import {
+  createNewConvo,
+  createNewMessage,
+  updateConvo,
+} from "../../redux/messageSlice";
 
 const Messages = () => {
-    const { userID } = useParams();
-    const user = useSelector((state: any) => state.user.userData);
-    const [messageText, setMessageText] = useState('');
-    const [recievedMessage, setRecievedMessage] = useState<any[]>([]);
-    const [mentions, setMentions] = useState([]);
-    const location = useLocation();
-    const dispatch = useDispatch<AppDispatch>()
-    const navigate = useNavigate()
+  const { userID } = useParams();
+  const user = useSelector((state: any) => state.user.userData);
+  const [messageText, setMessageText] = useState("");
+  const [recievedMessage, setRecievedMessage] = useState<any[]>([]);
+  const [mentions, setMentions] = useState([]);
+  const location = useLocation();
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    socket.off("response");
+    socket.on("response", (messageData: any) => {
+      setRecievedMessage((prev: any) => [...prev, messageData]);
+    });
+  }, []);
 
+  useEffect(() => {
+    const userID = user.userID;
+    socket.emit("userConnected", userID);
+  }, [socket]);
 
-    useEffect(() => {
-        socket.off('response');
-        socket.on('response', (messageData: any) => {
-            setRecievedMessage((prev: any) => [...prev, messageData]);
-        });
-    }, []);
+  useEffect(() => {
+    socket.on("getAllMessages", (messages: any[]) => {
+      setRecievedMessage(messages);
+    });
+  }, [socket]);
 
-    useEffect(() => {
-        const userID = user.userID;
-        socket.emit('userConnected', userID);
-    }, [socket]);
-
-    useEffect(() => {
-        socket.on('getAllMessages', (messages: any[]) => {
-            setRecievedMessage(messages);
-        });
-    }, [socket]);
-
-    const renderConversationList = () => {
-        if (location.pathname.includes('/messages/r/requests')) {
-            return <MessageRequestListContainer />;
-        } else if (
-            location.pathname === '/messages' ||
-            location.pathname.startsWith('/messages/')
-        ) {
-            return <ConversationListContainer />;
-        }
-        return null;
-    };
-
-
-    const renderNoConvoSelected = () => {
-        if (location.pathname === '/messages') {
-            return <NoConvoSelected />
-
-        }
-        return null
+  const renderConversationList = () => {
+    if (location.pathname.includes("/messages/r/requests")) {
+      return <MessageRequestListContainer />;
+    } else if (
+      location.pathname === "/messages" ||
+      location.pathname.startsWith("/messages/")
+    ) {
+      return <ConversationListContainer />;
     }
+    return null;
+  };
 
-    const renderNoMessageRequestSelected = () => {
-        if (location.pathname === '/messages/r/requests') {
-            return <NoMessageRequestSelected />
-        }
-        return null
+  const renderNoConvoSelected = () => {
+    if (location.pathname === "/messages") {
+      return <NoConvoSelected />;
     }
+    return null;
+  };
 
+  const renderNoMessageRequestSelected = () => {
+    if (location.pathname === "/messages/r/requests") {
+      return <NoMessageRequestSelected />;
+    }
+    return null;
+  };
 
-    useEffect(() => {
-        socket.on("sendMessage", (data: any) => {
+  useEffect(() => {
+    socket.on("sendMessage", (data: any) => {
+      const { conversation, message } = data;
+      dispatch(createNewMessage(message));
+      dispatch(updateConvo(conversation));
+    });
+  }, []);
 
-            const { conversation, message } = data
-            dispatch(createNewMessage(message))
-            dispatch(updateConvo(conversation))
-        })
-    }, [])
+  useEffect(() => {
+    socket.on("createNewMessage", (data: any) => {
+      const { conversation } = data;
 
-    useEffect(() => {
-        socket.on("createNewMessage", (data: any) => {
-            const { conversation } = data;
+      dispatch(createNewConvo(conversation));
+      navigate(`/messages/${conversation.conversationID}`);
+    });
+  }, []);
 
-            dispatch(createNewConvo(conversation));
-            navigate(`/messages/${conversation.conversationID}`);
-        });
-    }, []);
+  useEffect(() => {
+    socket.on("joinConversation", (data: any) => {
+      dispatch(updateConvo(data));
+    });
+  }, []);
 
-    useEffect(() => {
-        socket.on("joinConversation", (data: any) => {
-            dispatch(updateConvo(data))
-        })
-    }, [])
+  useEffect(() => {
+    socket.on("receiveMessage", (data: any) => {
+      const { conversation, message } = data;
+      dispatch(updateConvo(conversation));
+      dispatch(createNewMessage(message));
+    });
+  }, []);
 
-    useEffect(() => {
-        socket.on("receiveMessage", (data: any) => {
-            const { conversation, message } = data
-            dispatch(updateConvo(conversation))
-            dispatch(createNewMessage(message))
-        })
-    }, [])
-
-    return (
-        <div className="w-full relative flex-grow flex flex-col items-center justify-center gap-y-2 top-0 bottom-0">
-
-            <div className='z-[100] flex-grow flex lg:hidden w-full'>
-                <Routes>
-                    <Route path="/" element={<ConversationListContainer />} />
-                    <Route path="/new" element={<NewConversation />} />
-                    <Route path="/:conversationID" element={<ConversationContainer />} />
-                    <Route path="/r/requests" element={<MessageRequestListContainer />} />
-                    <Route path="/r/requests/:conversationID" element={<ConversationContainer />} />
-                </Routes>
-
-            </div>
-            <div className='hidden h-[100dvh] lg:flex w-full sticky top-0 bottom-0'>
-                <div className='min-w-[400px]'>
-                    {renderConversationList()}
-
-                </div>
-                {renderNoConvoSelected()}
-                {renderNoMessageRequestSelected()}
-                <Routes>
-                    <Route path="/new" element={<NewConversation />} />
-                    <Route path="/:conversationID" element={<ConversationContainer />} />
-                    <Route path="/r/requests/:conversationID" element={<ConversationContainer />} />
-                </Routes>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="w-full relative flex-grow flex flex-col items-center justify-center gap-y-2 top-0 bottom-0">
+      <div className="z-[100] h-screen flex-grow flex lg:hidden w-full">
+        <Routes>
+          <Route path="/" element={<ConversationListContainer />} />
+          <Route path="/new" element={<NewConversation />} />
+          <Route path="/:conversationID" element={<ConversationContainer />} />
+          <Route path="/r/requests" element={<MessageRequestListContainer />} />
+          <Route
+            path="/r/requests/:conversationID"
+            element={<ConversationContainer />}
+          />
+        </Routes>
+      </div>
+      <div className="hidden h-[100dvh] lg:flex w-full sticky top-0 bottom-0">
+        <div className="min-w-[400px]">{renderConversationList()}</div>
+        {renderNoConvoSelected()}
+        {renderNoMessageRequestSelected()}
+        <Routes>
+          <Route path="/new" element={<NewConversation />} />
+          <Route path="/:conversationID" element={<ConversationContainer />} />
+          <Route
+            path="/r/requests/:conversationID"
+            element={<ConversationContainer />}
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Messages;
