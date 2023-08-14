a64664ece58108b7c68111433657d954b73e96ec
jaynel29 <jaynellucanas@gmail.com>
2023-08-14 15:32:17 +0800

    Bugs in Text Are and Input Text for Poll Options, Posts, Replies and Reposts has been fixed

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,7 +1,6 @@
 /** @format */
 
 const developmentAddress = "http://127.0.0.1:5000";
-
 const producionAddress = "https://globally-express.onrender.com";
 
 export const serverAddress = producionAddress;
