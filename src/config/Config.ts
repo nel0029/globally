f40471de496fa2ad7fe881ee89a8b0bcce0519ea
jaynel29 <jaynellucanas@gmail.com>
2023-08-16 20:35:41 +0800

    Explore Routes has been added, Minor changes in some UI and Upload for images temporarily added

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -3,4 +3,4 @@
 const developmentAddress = "http://127.0.0.1:5000";
 const producionAddress = "https://globally-express.onrender.com";
 
-export const serverAddress = developmentAddress;
+export const serverAddress = producionAddress;
