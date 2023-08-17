399f74a2d6c517a16c5331225aaf8b00f122dacd
jaynel29 <jaynellucanas@gmail.com>
2023-08-17 22:29:18 +0800

    Navigation for profile, account settings has been changed

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC<HeaderProps> = ({ children, headerRef }) => {
   return (
     <div
       ref={headerRef}
-      className="z-30 w-full flex flex-row items-center bg-white border-b dark:border-b-0 dark:bg-Dark300 sticky top-0 gap-x-2 p-2 "
+      className="z-30 w-full flex flex-row items-center bg-white border-b dark:border-Dark400 dark:bg-Dark300 sticky top-0 gap-x-2 p-2 "
     >
       {children}
     </div>
